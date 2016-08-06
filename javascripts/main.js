fb24bfa427097f1cfa10ce6b8688b7a474eb261f
patrickcberry <patrickcberry@gmail.com>
2016-08-07 00:06:10 +1000

    General update

--- javascripts/main.js
+++ javascripts/main.js
@@ -41,9 +41,9 @@ function initMap() {
 
 	google.maps.event.addListener('marker' + i,'click', function(){
 		console.log('Marker clicked');
-		console.log(marker);
-		console.log(marker.title);
-		console.log(marker.content);
+		//console.log(marker);
+		console.log(this.title);
+		console.log(this.content);
 		//infoWindow.setContent(marker.content);
 		//infoWindow.open(map,marker);
 	});
