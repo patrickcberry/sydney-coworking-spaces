406080e37a1b9e807e83817dfbbb3de7e615d61d
patrickcberry <patrickcberry@gmail.com>
2016-08-06 18:16:30 +1000

    Added map demo

--- updateall.sh
+++ updateall.sh
@@ -1,1 +1,13 @@
-git add . | git commit -a -m $1  | git push
+#!/bin/sh
+
+# Add all changed files to git and push to GitHub
+#
+# Accepts a single parameter as the commit comment
+
+if [ $# == 1 ]; then
+	comment=$1
+else
+	comment="General update"
+fi
+
+git add . && git commit -a -m "$comment" && git push
