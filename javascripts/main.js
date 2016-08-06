be40550a06ab20aa22a4b4d88173fe1a5038ff0b
patrickcberry <patrickcberry@gmail.com>
2016-08-06 22:35:37 +1000

    General update

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,26 @@
 console.log('This would be the main JS file.');
 
+
+var locationData = {
+	{
+		title: 		"Home",
+		position: 	{lat: -33.714056, lng: 151.054259},
+	},
+	{
+		title: 		"Pyrmont",
+		position: 	{lat: -33.868641, lng: 151.196729},
+	},
+	{
+		title: 		"Paramatta",
+		position: 	{lat: -33.813676, lng: 151.007102},
+	}
+}
+
+
+// -33.868641, 151.196729 Pyrmont
+// -33.813676, 151.007102 Paramatta
+
+
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
@@ -8,9 +29,13 @@ function initMap() {
   });
 
   var marker = new google.maps.Marker({
-    position: {lat: -33.714056, lng: 151.054259},
+
+  	position: locationData[1].position,
+  	title: locationData[1].title,
+
+    //position: {lat: -33.714056, lng: 151.054259},
+    //title: "Home\n34 Wisteria Crescent\nCherrybrook"
     map: map,
-    title: "HomeX \n34 Wisteria Crescent\nCherrybrook"
   });  
 
 }
