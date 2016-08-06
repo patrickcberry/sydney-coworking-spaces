f350e83333eb80eb396dd02455f35889cb04617a
patrickcberry <patrickcberry@gmail.com>
2016-08-06 22:19:33 +1000

    General update

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
 console.log('This would be the main JS file.');
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -33.799, lng: 151.006},
+    zoom: 11
+  });
+
+  var marker = new google.maps.Marker({
+    position: {lat: -33.714056, lng: 151.054259},
+    map: map,
+    title: "Home"
+  });  
+
+}
