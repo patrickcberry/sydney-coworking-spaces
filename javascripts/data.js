b7c8ef6df24cb1d50539a8e82ed91c5dd9ad7952
patrickcberry <patrickcberry@gmail.com>
2016-08-07 15:55:22 +1000

    WIP Loading inital data

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,22 +1,45 @@
 var locationData = [];
 
 locationData.push({
-		title: 		"Home",
-		position: 	{lat: -33.714056, lng: 151.054259},
-		content:    "Home content"
+		title: 		"Bay 9",
+		position: 	{lat: -33.842835, lng: 151.210405},
+		content:    "Bay 9",
+		addess: 	"Bay 9 Middlemiss Street, Lavender Bay NSW 2060",
+		url: 		"http://bay9.com.au"
 });
 
 locationData.push({
-		title: 		"Pyrmont",
-		position: 	{lat: -33.868641, lng: 151.196729},
-		content:    "Pyrmont content"
+		title: 		"WOTSO WorkSpace",
+		position: 	{lat: -33.862894, lng: 151.088812},
+		content:    "WOTSO WorkSpace (North Strathfield)",
+		address: 	"Level 2, 5 George St, North Strathfield, NSW 2137",
+		url: 		"http://www.wotsoworkspace.com.au/"
 });
 
 locationData.push({
-		title: 		"Paramatta",
-		position: 	{lat: -33.813676, lng: 151.007102},
-		content:    "Paramatta content"
+		title: 		"WOTSO WorkSpace",
+		position: 	{lat: -33.831806, lng: 151.223375},
+		content:    "WOTSO WorkSpace (Neutral Bay)",
+		address: 	"Level 1, 50 Yeo Street, Neutral Bay NSW 2089",
+		url: 		"http://www.wotsoworkspace.com.au/"
 });
+
+locationData.push({
+		title: 		"WOTSO WorkSpace",
+		position: 	{lat: -33.872046, lng: 151.194601 },
+		content:    "WOTSO WorkSpace (Pyrmont)",
+		address: 	"Level 3, 55 Pyrmont Bridge Road, Pyrmont, NSW",
+		url: 		"http://www.wotsoworkspace.com.au/"
+});
+
+locationData.push({
+		title: 		"WOTSO WorkSpace",
+		position: 	{lat: -33.753232, lng: 150.694554 },
+		content:    "WOTSO WorkSpace (Penrith)",
+		address: 	"95b Station Street, Penrith, NSW 2750",
+		url: 		"http://www.wotsoworkspace.com.au/"
+});
+
 
 locationData.push({
 		title: 		"Fishburners",
