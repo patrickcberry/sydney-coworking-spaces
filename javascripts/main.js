6ff978c346cc2b973be651c582ed018442288d09
patrickcberry <patrickcberry@gmail.com>
2016-08-07 14:05:18 +1000

    Split data into seperate file

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,23 +1,3 @@
-// Below is the data for each co-working space location
-
-var locationData = [
-	{
-		title: 		"Home",
-		position: 	{lat: -33.714056, lng: 151.054259},
-		content:    "Home content"
-	},
-	{
-		title: 		"Pyrmont",
-		position: 	{lat: -33.868641, lng: 151.196729},
-		content:    "Pyrmont content"
-	},
-	{
-		title: 		"Paramatta",
-		position: 	{lat: -33.813676, lng: 151.007102},
-		content:    "Paramatta content"
-	}
-]
-
 // Below is the script to create and display the map using the 
 // GoogleMaps API
 
